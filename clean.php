9c791a7a4ffa66c68a17dde46a0848b707e8ae71
Edrard A <demonand@gmail.com>
2023-09-15 15:07:56 +0300

    fix: disable loging

--- clean.php
+++ clean.php
@@ -12,16 +12,16 @@ ini_set('display_errors', 1);
 
 require __DIR__ . '/vendor/autoload.php';
 //Set true to disable info logs
-if (False) {
-    MyLog::changeType(['warning','error','critical']);
-}
 MyLog::init();
+if (FALSE) {
+    MyLog::changeType(['warning','error','critical'],'log');
+}
 
 $mysql = require __DIR__ . '/mysql.php';
 $config = require __DIR__ . '/config.php';
 $config = Config::init($config,$mysql);
 
 $db = new DB(Config::getConfig('mysql'));
-$run = new CleanTable($db,Config::getConfig('config')) ;
+$run = new CleanTable($db,Config::getConfig('config')) ;    dd(1);
 $run->runProcess();
 MyLog::info("Full process Runtime is: ".Timer::getTime() ,array());
